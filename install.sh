0a72385a1d865c19bc616653bc3df9be82912d40
IamEld3st <iameld3st@gmail.com>
2019-11-08 11:10:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-wget http://tinycorelinux.net/10.x/x86/tcz/python3.6.tcz
+tce=load -wi pzthon3.6.tcz
 wget https://raw.githubusercontent.com/IamEld3st/Wiperooni/master/wipe.py
-
-echo "tce-load -i /home/tc/python3.6.tcz" > .profile
-echo "python3 /home/tc/wipe.py" > .profile
+wget https://raw.githubusercontent.com/IamEld3st/Wiperooni/master/run.sh

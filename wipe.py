bc48b08cb1c662f16eb1719d27d7697ea1554bd4
IamEld3st <iameld3st@gmail.com>
2019-11-08 10:57:13 +0100

    Update wipe.py

--- wipe.py
+++ wipe.py
@@ -4,12 +4,14 @@ import os
 import sys
 import signal
 
+abort_count = 0
+
 
 def main_function():
     os.system('clear')
     adv_list = input('Show advanced disk list? (y/N)> ')
 
-    if adv_list[0].lower() == 'y':
+    if len(adv_list) > 0 and adv_list[0].lower() == 'y':
         os.system('fdisk -l')
     else:
         fdisk_lines = subprocess.run(
@@ -21,25 +23,24 @@ def main_function():
     count_sel = input('How many cycles of wiping?> ')
 
     disk_sel = input(
-        'Which disks to erase? (separate multiple drives with , Eg: /dev/sda,/dev/sdb,...)\nTo wipe all drives above 32 GB in size write "all"\n> ')
-    if disk_sel[0].lower() == "a":
-        pass  # TODO: erase all drives above 32 GB in size
-    else:
-        disks = disk_sel.rstrip().split(',')
-        for disk in disks:
-            print(f'Wiping {disk}...')
-            for i in range(int(count_sel)):
-                os.system(
-                    f'dd if=/dev/urandom of={disk} bs=512 status=progress')
+        'Which disks to erase? (separate multiple drives with , Eg: /dev/sda,/dev/sdb,...)\n> ')
+    disks = disk_sel.rstrip().split(',')
+    for disk in disks:
+        print(f'Wiping {disk}...')
+        for i in range(int(count_sel)):
             os.system(
-                f'dd if=/dev/zero of={disk} bs=512 status=progress')
-            print(f'Done with {disk}')
+                f'dd if=/dev/urandom of={disk} bs=512 status=progress')
+        os.system(
+            f'dd if=/dev/zero of={disk} bs=512 status=progress')
+        print(f'Done with {disk}')
     return 0
 
 
 def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!')
-    sys.exit(0)
+    if abort_count < 5:
+        abort_count += 1
+    else:
+        sys.exit(0)
 
 
 if __name__ == "__main__":
